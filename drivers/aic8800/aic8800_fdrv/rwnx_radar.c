c1077828c27575160ddb56375bbbc7ef8a5945f5
Nathaniel Ephraim Canilang <nathcanilang@gmail.com>
2025-10-25 07:04:43 +0800

    added nested if arguments

--- drivers/aic8800/aic8800_fdrv/rwnx_radar.c
+++ drivers/aic8800/aic8800_fdrv/rwnx_radar.c
@@ -1397,15 +1397,11 @@ static void rwnx_radar_cac_work(struct work_struct *ws)
     ctxt = &rwnx_hw->chanctx_table[radar->cac_vif->ch_index];
     #if (LINUX_VERSION_CODE >= KERNEL_VERSION(6,9,0))
     cfg80211_cac_event(radar->cac_vif->ndev,
-                    #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)
-                       &ctxt->chan_def,
-                    #endif
-                       NL80211_RADAR_CAC_FINISHED, GFP_KERNEL);
+                    &ctxt->chan_def,
+                    NL80211_RADAR_CAC_FINISHED, GFP_KERNEL);
     #else
     cfg80211_cac_event(radar->cac_vif->ndev,
-            #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)
                 &ctxt->chan_def,
-            #endif
                 NL80211_RADAR_CAC_FINISHED);
 
     rwnx_send_apm_stop_cac_req(rwnx_hw, radar->cac_vif);
@@ -1505,15 +1501,12 @@ void rwnx_radar_cancel_cac(struct rwnx_radar *radar)
         rwnx_send_apm_stop_cac_req(rwnx_hw, radar->cac_vif);
         #if (LINUX_VERSION_CODE >= KERNEL_VERSION(6,9,0))
         cfg80211_cac_event(radar->cac_vif->ndev,
-                        #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)
-                           &ctxt->chan_def,
-                        #endif
-                           NL80211_RADAR_CAC_ABORTED, GFP_KERNEL);
+                            &ctxt->chan_def,
+                            NL80211_RADAR_CAC_ABORTED, 
+                            GFP_KERNEL);
             #else
         cfg80211_cac_event(radar->cac_vif->ndev,
-                #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)
                     &ctxt->chan_def,
-                #endif
                     NL80211_RADAR_CAC_ABORTED);
 
         rwnx_chanctx_unlink(radar->cac_vif);
